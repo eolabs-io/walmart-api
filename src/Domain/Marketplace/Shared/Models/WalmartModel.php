2536608bc26e3e8161f89242cd7549f07a4dec6c
tim hall <timhall@tims-mbp.lan>
2021-03-30 15:57:05 -0500

    Fixed type with WalmartModel trait

--- src/Domain/Marketplace/Shared/Models/WalmartModel.php
+++ src/Domain/Marketplace/Shared/Models/WalmartModel.php
@@ -3,7 +3,7 @@ namespace EolabsIo\WalmartApi\Domain\Marketplace\Shared\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use EolabsIo\WalmartApi\Domain\Marketplace\Shared\Models\Concerns\RequestBodyAble;
+use EolabsIo\WalmartApi\Domain\Marketplace\Shared\Models\Concerns\RequestBodyable;
 
 abstract class WalmartModel extends Model
 {
